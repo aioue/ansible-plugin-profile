ba01cf608fff04bc19b9fcc83e5e4184ba7c61d7
Tom Paine <tom.paine@ontrackretail.co.uk>
2015-06-12 17:15:28 +0100

    initial

---
+++ callback_plugins/profile_tasks.py
@@ -0,0 +1,93 @@
+import time
+
+from ansible.callbacks import display
+
+
+# define start time
+t0 = tn = time.time()
+
+
+def secondsToStr(t):
+    # http://bytes.com/topic/python/answers/635958-handy-short-cut-formatting-elapsed-time-floating-point-seconds
+    rediv = lambda ll, b: list(divmod(ll[0], b)) + ll[1:]
+    return "%d:%02d:%02d.%03d" % tuple(reduce(rediv, [[t * 1000, ], 1000, 60, 60]))
+
+
+def filled(msg, fchar="*"):
+    if len(msg) == 0:
+        width = 79
+    else:
+        msg = "%s " % msg
+        width = 79 - len(msg)
+    if width < 3:
+        width = 3
+    filler = fchar * width
+    return "%s%s " % (msg, filler)
+
+
+def timestamp(self):
+    if self.current is not None:
+        self.stats[self.current] = time.time() - self.stats[self.current]
+
+
+def tasktime():
+    global tn
+    time_current = time.strftime('%A %d %B %Y  %H:%M:%S %z')
+    time_elapsed = secondsToStr(time.time() - tn)
+    time_total_elapsed = secondsToStr(time.time() - t0)
+    display(filled('%s (%s)%s%s' % (time_current, time_elapsed, ' ' * 7, time_total_elapsed)))
+    tn = time.time()
+
+
+class CallbackModule(object):
+    """
+    A plugin for timing tasks
+    """
+
+    def __init__(self):
+        self.stats = {}
+        self.current = None
+
+    def playbook_on_task_start(self, name, is_conditional):
+        """
+        Logs the start of each task
+        """
+        tasktime()
+        timestamp(self)
+
+        # Record the start time of the current task
+        self.current = name
+        self.stats[self.current] = time.time()
+
+    def playbook_on_setup(self):
+        tasktime()
+
+    def playbook_on_stats(self, stats):
+        tasktime()
+        display(filled("", fchar="="))
+
+        """
+        Prints the timings
+        """
+        # Record the timing of the very last task
+        timestamp(self)
+
+        # Sort the tasks by their running time
+        results = sorted(
+            self.stats.items(),
+            key=lambda value: value[1],
+            reverse=True,
+        )
+
+        # Just keep the top 20
+        results = results[:20]
+
+        # Print the timings
+        for name, elapsed in results:
+            print(
+                "{0:-<70}{1:->9}".format(
+                    '{0} '.format(name),
+                    ' {0:.02f}s'.format(elapsed),
+                )
+            )
+        print ''
